74ec57257c4ab6c7442b6388c3e3b1bf9a773d53
Hugo Tessier <hugo.tessier-huort.ext@geodis.com>
2022-01-31 18:20:03 +0100

    made change on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <p>
-            coucou la <code>src/App.js</code> and save to reload.
+            Just a change la <code>src/App.js</code> and save to reload.
           </p>
           <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
             Learn React
