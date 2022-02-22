d4377b0c8dda74b914dcc862a2a98c467e03edf6
Hugo Tessier <hugo.tessier-huort.ext@geodis.com>
2022-02-22 12:42:55 +0100

    test protect master branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <p>
-            Coucou le nouveau merge <code>src/App.js</code> and save to reload.
+            Coucou le nouveau merge <code>src/App.js</code> and save to reload. Test protect master branch
           </p>
           <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
             Learn React
